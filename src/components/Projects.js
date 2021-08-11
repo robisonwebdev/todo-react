f5eee15c73a1827c608ddc9e64c66ba770539d43
robisonwebdev <david@robisonweb.dev>
2021-08-11 10:22:15 -0600

    Imported Projects.css. Gave div an ID of Projects.

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -1,8 +1,9 @@
 import React from 'react';
+import '../styles/Projects.css';
 
 const Projects = () => {
     return (
-        <div>Projects</div>
+        <div id='Projects'>Projects</div>
     );
 }
 
