13cb64693a8051b8f4d1e2a2b3b2eda977069846
robisonwebdev <david@robisonweb.dev>
2021-08-11 10:15:26 -0600

    Imported Lists. Added Lists to Main.

--- src/components/Main.js
+++ src/components/Main.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import Projects from './Projects';
+import Lists from './Lists';
 
 const Main = () => {
     return (
         <div id='Main'>
             <Projects />
+            <Lists />
         </div>
     );
 }
