2885acf307aaebff80a4b6d00f333b20b24a7143
robisonwebdev <david@robisonweb.dev>
2021-08-11 10:03:33 -0600

    Imported Footer. Added Footer to App.

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import Header from './components/Header';
 import Main from './components/Main';
+import Footer from './components/Footer';
 
 const App = () => {
   return (
     <div>
       <Header />
       <Main />
+      <Footer />
     </div>
   );
 }
