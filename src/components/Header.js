7f11e0f97b4e82fd71632f270648a9ca2a6a32b0
robisonwebdev <david@robisonweb.dev>
2021-08-12 14:56:36 -0600

    Fixed class typo

--- src/components/Header.js
+++ src/components/Header.js
@@ -4,7 +4,7 @@ import '../styles/Header.css';
 const Header = () => {
     return (
         <div id='Header'>
-            <i class="las la-check-circle"></i>
+            <i className="las la-check-circle"></i>
             <h1>Todo List</h1>
         </div>
     );
