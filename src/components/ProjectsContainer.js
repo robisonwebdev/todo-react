e41ee93adb7eb82376d435cb89adccec3c250126
robisonwebdev <david@robisonweb.dev>
2021-08-13 17:09:04 -0600

    Imported Projects. Added h2 with text of Projects. Added temp Projects component.

--- src/components/ProjectsContainer.js
+++ src/components/ProjectsContainer.js
@@ -1,10 +1,13 @@
 import React from 'react';
+import Projects from './Projects';
 import '../styles/ProjectsContainer.css';
 
 const ProjectsContainer = () => {
     return (
         <div id='ProjectsContainer'>
             <i class="las la-inbox">All Projects</i>
+            <h2>Projects</h2>
+            <Projects />
         </div>
     );
 }
