54cd1a1c46253e0fefd159a1bb7bbcced12798af
robisonwebdev <david@robisonweb.dev>
2021-08-14 14:25:50 -0600

    Imported useState. Setup state for inputValue, setInputValue. Created function handleChanged for input field. Created function handleSubmit for form submit. Created form with input and button.

--- src/components/ProjectsContainer.js
+++ src/components/ProjectsContainer.js
@@ -1,15 +1,29 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Project from './Project';
 import '../styles/ProjectsContainer.css';
 
-const ProjectsContainer = ({ projects, setProjects }) => {
+const ProjectsContainer = ({ projects, addProject }) => {
+    const [inputValue, setInputValue] = useState('');
     const projectsMap = projects.map(project => <Project text={project.title} />)
+
+    const handleChange = (event) => {
+        setInputValue(event.target.value);
+    }
+
+    const handleSubmit = (event) => {
+        event.preventDefault();
+        addProject(inputValue);
+    }
 
     return (
         <div id='ProjectsContainer'>
             <i className="las la-inbox">All Projects</i>
             <h2>Projects</h2>
             {projectsMap}
+            <form onSubmit={handleSubmit}>
+                <input type='text' value={inputValue} onChange={handleChange} />
+                <button type='submit'>Add Item</button>
+            </form>
         </div>
     );
 }
