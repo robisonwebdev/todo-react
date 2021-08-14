fdc0a3d032cd9cbe0f85d5ef4cbe887dad781e99
robisonwebdev <david@robisonweb.dev>
2021-08-14 13:59:08 -0600

    Imported useState. Setup projects state.

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Header from './components/Header';
 import Main from './components/Main';
 import Footer from './components/Footer';
 import './styles/App.css';
 
 const App = () => {
+  const [projects, setProjects] = useState({});
+
   return (
     <div id='App'>
       <Header />
