35888bc17f8aa7f3ddbe103fdb3e7852ef8c2084
robisonwebdev <david@robisonweb.dev>
2021-08-14 15:52:22 -0600

    Added destructured prop onCancel. Added onCancel to the onClick for Cancel button.

--- src/components/AddProjectForm.js
+++ src/components/AddProjectForm.js
@@ -1,11 +1,11 @@
 import React from 'react';
 
-const AddProjectForm = ({ onSubmit, onChange, value}) => {
+const AddProjectForm = ({ onSubmit, onChange, onCancel, value}) => {
     return (
         <form onSubmit={onSubmit}>
                 <input type='text' value={value} onChange={onChange} />
                 <button type='submit'>Add Item</button>
-                <button type='reset'>Cancel</button>
+                <button type='button' onClick={onCancel}>Cancel</button>
         </form>
     );
 }
