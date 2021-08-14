2fd397319c2de21d3bee01d54574737b1fe840c7
robisonwebdev <david@robisonweb.dev>
2021-08-14 14:53:04 -0600

    Destructured props. Cut and pasted form from ProjectContainer.js into AddProjectForm.js

--- src/components/AddProjectForm.js
+++ src/components/AddProjectForm.js
@@ -1,7 +1,13 @@
 import React from 'react';
 
-const AddProjectForm = () => {
-    return ();
+const AddProjectForm = ({ onSubmit, onChange, value}) => {
+    return (
+        <form onSubmit={handleSubmit}>
+                <input type='text' value={inputValue} onChange={handleChange} />
+                <button type='submit'>Add Item</button>
+                <button type='reset'>Cancel</button>
+        </form>
+    );
 }
 
 export default AddProjectForm;
