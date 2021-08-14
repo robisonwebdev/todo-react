d42289db314713f647ff4451ca30a2f249261a43
robisonwebdev <david@robisonweb.dev>
2021-08-14 14:08:30 -0600

    Destructured props. Setup projectsMap to map through projects and return each project to Projects component

--- src/components/ProjectsContainer.js
+++ src/components/ProjectsContainer.js
@@ -2,12 +2,14 @@ import React from 'react';
 import Project from './Project';
 import '../styles/ProjectsContainer.css';
 
-const ProjectsContainer = () => {
+const ProjectsContainer = ({ projects, setProjects }) => {
+    const projectsMap = projects.map(project => <Project text={project.title} />)
+
     return (
         <div id='ProjectsContainer'>
             <i className="las la-inbox">All Projects</i>
             <h2>Projects</h2>
-            <Project text='Shopping' />
+            {projectsMap}
         </div>
     );
 }
