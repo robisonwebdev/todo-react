8bf8462071ab9cc320b495bcc6813c00f235fcb4
robisonwebdev <david@robisonweb.dev>
2021-08-15 15:40:41 -0600

    Created variable to map though projectList items and return which in a li.

--- src/components/Lists.js
+++ src/components/Lists.js
@@ -2,9 +2,16 @@ import React from 'react';
 import '../styles/Lists.css';
 
 const Lists = ({ projectList }) => {
+    const projectItems = projectList[0].items.map(item => {
+        return <li>{item}</li>
+    })
+    
     return (
         <div id='Lists'>
             <h1>{projectList[0].title}</h1>
+            <ul>
+                {projectItems}
+            </ul>
         </div>
     );
 }
