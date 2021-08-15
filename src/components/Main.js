dc379b1daa16327147132dc3a1aa3b07572442d3
robisonwebdev <david@robisonweb.dev>
2021-08-15 15:06:13 -0600

    Imported useState. Cut code from App.js(projects, setProjects, addProject) and pasted into Main.js

--- src/components/Main.js
+++ src/components/Main.js
@@ -1,9 +1,26 @@
-import React from 'react';
+import React, { useState } from 'react';
 import ProjectsContainer from './ProjectsContainer';
 import Lists from './Lists';
 import '../styles/Main.css';
 
-const Main = ({ projects, addProject}) => {
+const Main = () => {
+    const [projects, setProjects] = useState([
+        {
+          title: 'Fishing',
+        },
+        {
+          title: 'Job',
+        }
+    ]);
+    
+    const addProject = (value) => {
+        let newObject = {
+            title: value,
+        }
+
+        setProjects(prevState => [...prevState, newObject])
+    }
+
     return (
         <div id='Main'>
             <ProjectsContainer projects={projects} addProject={addProject} />
