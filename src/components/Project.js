8291010458fb882b1ec257c90718bbd39a4920af
robisonwebdev <david@robisonweb.dev>
2021-08-18 16:03:00 -0600

    Destructured deleteProject and projectID. On icon la-trash, setup onClick event to execute deleteProject.

--- src/components/Project.js
+++ src/components/Project.js
@@ -1,14 +1,14 @@
 import React from 'react';
 import '../styles/Project.css';
 
-const Project = ({ text, onClick }) => {
+const Project = ({ text, deleteProject, projectID, onClick }) => {
     return (
         <div className='Project'>
             <div className='projectItem' onClick={() => onClick(text)}>
                 <i className="las la-tasks"></i>
                 <p>{text}</p>
             </div>            
-            <i className="las la-trash"></i>
+            <i className="las la-trash" onClick={() => deleteProject(projectID)}></i>
         </div>
     );
 }
