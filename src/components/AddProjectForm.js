4ce161376b5f62fee0e1fe967b9c8c905a5e2259
robisonwebdev <david@robisonweb.dev>
2021-08-19 08:25:31 -0600

    Gave form a className of projectForm

--- src/components/AddProjectForm.js
+++ src/components/AddProjectForm.js
@@ -3,7 +3,7 @@ import '../styles/AddProjectForm.css';
 
 const AddProjectForm = ({ onSubmit, onChange, onCancel, value}) => {
     return (
-        <form onSubmit={onSubmit}>
+        <form className='projectForm' onSubmit={onSubmit}>
                 <input type='text' value={value} onChange={onChange} />
                 <button type='submit'>Add Item</button>
                 <button type='button' onClick={onCancel}>Cancel</button>
