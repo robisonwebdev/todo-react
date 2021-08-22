61d7ce9ddbca9d2d461f77745ecffa7181b2ce1c
robisonwebdev <david@robisonweb.dev>
2021-08-21 18:35:10 -0600

    Changed onClick prop to viewProject.

--- src/components/Project.js
+++ src/components/Project.js
@@ -1,10 +1,10 @@
 import React from 'react';
 import '../styles/Project.css';
 
-const Project = ({ text, deleteProject, projectID, onClick }) => {
+const Project = ({ text, deleteProject, projectID, viewProject }) => {
     return (
         <div className='Project'>
-            <div className='projectItem' onClick={() => onClick(text)}>
+            <div className='projectItem' onClick={() => viewProject(projectID)}>
                 <i className="las la-tasks"></i>
                 <p>{text}</p>
             </div>            
