9987dfa1799e3ea3c436b4267ae0e3b276618bd4
robisonwebdev <david@robisonweb.dev>
2021-08-25 13:21:22 -0600

    Removed file

--- src/components/AddItemBtn.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-
-const AddItemBtn = ({ className, onClick }) => {
-    return (
-        <button className={className} onClick={onClick}>Add Item</button>
-    );
-}
-
-export default AddItemBtn;
