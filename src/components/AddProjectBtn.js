40e3d1b51afff1d72ebf52bb011089187c12e5ef
robisonwebdev <david@robisonweb.dev>
2021-08-25 13:33:19 -0600

    Removed file.

--- src/components/AddProjectBtn.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-
-const AddProjectBtn = ({ onClick }) => {
-    return (
-        <i className="las la-plus" onClick={onClick}>Add Project</i>
-    );
-}
-
-export default AddProjectBtn;
