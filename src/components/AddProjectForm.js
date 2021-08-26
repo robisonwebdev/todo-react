1dcc1e14309eb3f6e8f8a2210caeca46ec56cc06
robisonwebdev <david@robisonweb.dev>
2021-08-26 14:08:58 -0600

    Added placeholder on input

--- src/components/AddProjectForm.js
+++ src/components/AddProjectForm.js
@@ -4,7 +4,7 @@ import '../styles/AddProjectForm.css';
 const AddProjectForm = ({ onSubmit, onChange, onCancel, value}) => {
     return (
         <form className='projectForm' onSubmit={onSubmit}>
-                <input type='text' value={value} onChange={onChange} />
+                <input type='text' value={value} onChange={onChange} placeholder='Project Name' />
                 <div className='projectFormBtns'>
                     <button type='submit'>Add Item</button>
                     <button type='button' onClick={onCancel}>Cancel</button>
