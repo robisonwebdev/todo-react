2280ccd1c4781b256f1651bc27800f10079b06fb
robisonwebdev <david@robisonweb.dev>
2021-08-27 16:32:29 -0600

    Changed text for sumbit button

--- src/components/AddProjectForm.js
+++ src/components/AddProjectForm.js
@@ -6,7 +6,7 @@ const AddProjectForm = ({ onSubmit, onChange, onCancel, value}) => {
         <form className='projectForm' onSubmit={onSubmit}>
                 <input type='text' value={value} onChange={onChange} placeholder='Project Name' />
                 <div className='projectFormBtns'>
-                    <button type='submit'>Add Item</button>
+                    <button type='submit'>Add Project</button>
                     <button type='button' onClick={onCancel}>Cancel</button>
                 </div>
         </form>
