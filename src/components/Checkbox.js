111fa637763377db68bb519e15d4156756b5fa4b
robisonwebdev <david@robisonweb.dev>
2021-08-29 10:47:06 -0600

    Changed className on label to accept a prop of className.

--- src/components/Checkbox.js
+++ src/components/Checkbox.js
@@ -1,8 +1,8 @@
 import React from 'react';
 
-const Checkbox = ({ checked, id, name, updateChecked,  }) => {
+const Checkbox = ({ className, checked, id, name, updateChecked  }) => {
     return (
-        <label className='item'>
+        <label className={className}>
             <input type='checkbox' checked={checked} onChange={() => updateChecked(id, name)} />
             {name}
         </label>
