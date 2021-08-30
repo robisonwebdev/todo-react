b840593bc44e63b4239d5baebc22d26201abca5d
robisonwebdev <david@robisonweb.dev>
2021-08-30 16:39:05 -0600

    Cut and copied code from List.js into this file.

--- src/components/DisplayChecked.js
+++ src/components/DisplayChecked.js
@@ -2,7 +2,14 @@ import React from 'react';
 
 const DisplayChecked = () => {
     return (
-        <div></div>
+        <div className='displayChecked'>
+            <h1>Completed</h1>
+            <div className='displayCheckedBtns'>
+                <Button className='checkedButtons' onClick={() => uncheckAll(id)}  text='Uncheck All' />
+                <Button className='checkedButtons' text='Delete All' />
+            </div>
+            {displayChecked}
+        </div>
     );
 }
 
