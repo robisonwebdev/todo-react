f0aaf3007976e8b366dbf48ecea5ecf1e2b635b1
robisonwebdev <david@robisonweb.dev>
2021-08-30 17:03:47 -0600

    Copy and pasted displayChecked variable from List.js into this file. Updated props.

--- src/components/DisplayChecked.js
+++ src/components/DisplayChecked.js
@@ -1,12 +1,19 @@
 import React from 'react';
+import Checkbox from './Checkbox';
 import Button from './Button';
 
-const DisplayChecked = ({ displayChecked, uncheckAll }) => {
+const DisplayChecked = ({ id, items, uncheckAll, updateChecked }) => {
+    let displayChecked = items.map(item => {
+        if (item.checked) {
+            return <Checkbox className='checkedItem' key={item.name + id} name={item.name} checked={item.checked} updateChecked={updateChecked} id={id} />
+        } else return null;
+    });
+
     return (
         <div className='displayChecked'>
             <h1>Completed</h1>
             <div className='displayCheckedBtns'>
-                <Button className='checkedButtons' onClick={() => uncheckAll(id)}  text='Uncheck All' />
+                <Button className='checkedButtons' onClick={uncheckAll}  text='Uncheck All' />
                 <Button className='checkedButtons' text='Delete All' />
             </div>
             {displayChecked}
