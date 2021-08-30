1966ff476d70409085fbe43956f423f46c7a8171
robisonwebdev <david@robisonweb.dev>
2021-08-30 17:01:39 -0600

    Imported DisplayChecked. Move displayChecked variable to DisplayChecked component. Added DisplayChecked(with props) to return

--- src/components/Lists.js
+++ src/components/Lists.js
@@ -1,6 +1,7 @@
 import React, { useEffect, useState } from 'react';
 import Checkbox from './Checkbox';
 import Button from './Button';
+import DisplayChecked from './DisplayChecked';
 import AddItemForm from './AddItemForm';
 import '../styles/Lists.css';
 
@@ -17,14 +18,7 @@ const Lists = ({ addItem, id, items, title, uncheckAll, updateChecked }) => {
         if (!item.checked) {
             return <Checkbox className='item' key={item.name + id} name={item.name} checked={item.checked} updateChecked={updateChecked} id={id} />
         } else return null;        
-    });
-
-
-    let displayChecked = items.map(item => {
-        if (item.checked) {
-            return <Checkbox className='checkedItem' key={item.name + id} name={item.name} checked={item.checked} updateChecked={updateChecked} id={id} />
-        } else return null;
-    })
+    });    
 
     const handleAddItemBtn = () => {
         setShowBtn(false);
@@ -57,14 +51,7 @@ const Lists = ({ addItem, id, items, title, uncheckAll, updateChecked }) => {
                 {showBtn ? <Button onClick={handleAddItemBtn} className='listButton' text='Add Item' /> : null}
                 {showForm ? <AddItemForm submit={handleSubmit} cancel={handleReset} handleChange={handleChange} value={inputValue} /> : null}
             </div>            
-            <div className='displayChecked'>
-                <h1>Completed</h1>
-                <div className='displayCheckedBtns'>
-                    <Button className='checkedButtons' onClick={() => uncheckAll(id)}  text='Uncheck All' />
-                    <Button className='checkedButtons' text='Delete All' />
-                </div>
-                {displayChecked}
-            </div>
+            <DisplayChecked id={id} items={items} uncheckAll={() => uncheckAll(id)} updateChecked={updateChecked} />
         </div>
     );
 }
