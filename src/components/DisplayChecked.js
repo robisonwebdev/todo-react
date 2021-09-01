873969ec8fe492c0da3450a7c870d2846c963e39
robisonwebdev <david@robisonweb.dev>
2021-08-31 18:35:15 -0600

    Added prop of deleteChecked. Added deleteChecked to onClick event on Delete All button.

--- src/components/DisplayChecked.js
+++ src/components/DisplayChecked.js
@@ -2,7 +2,7 @@ import React from 'react';
 import Checkbox from './Checkbox';
 import Button from './Button';
 
-const DisplayChecked = ({ id, items, uncheckAll, updateChecked }) => {
+const DisplayChecked = ({ id, items, uncheckAll, deleteChecked, updateChecked }) => {
     let displayChecked = items.map(item => {
         if (item.checked) {
             return <Checkbox className='checkedItem' key={item.name + id} name={item.name} checked={item.checked} updateChecked={updateChecked} id={id} />
@@ -14,7 +14,7 @@ const DisplayChecked = ({ id, items, uncheckAll, updateChecked }) => {
             <h1>Completed</h1>
             <div className='displayCheckedBtns'>
                 <Button className='checkedButtons' onClick={uncheckAll}  text='Uncheck All' />
-                <Button className='checkedButtons' text='Delete All' />
+                <Button className='checkedButtons' onClick={deleteChecked} text='Delete All' />
             </div>
             {displayChecked}
         </div>
