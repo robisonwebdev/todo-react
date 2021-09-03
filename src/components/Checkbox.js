329abeb6449d79fff67ee0da6ab762af9be7d036
robisonwebdev <david@robisonweb.dev>
2021-09-02 18:28:35 -0600

    Added two div's with classNames of itemLeft and itemRight. Moved input and {name} into itemLeft.

--- src/components/Checkbox.js
+++ src/components/Checkbox.js
@@ -3,8 +3,11 @@ import React from 'react';
 const Checkbox = ({ className, checked, id, name, updateChecked  }) => {
     return (
         <label className={className}>
-            <input type='checkbox' checked={checked} onChange={() => updateChecked(id, name)} />
-            {name}
+            <div className='itemLeft'>
+                <input type='checkbox' checked={checked} onChange={() => updateChecked(id, name)} />
+                {name}
+            </div>
+            <div className='itemRight'></div>
         </label>
     );
 }
