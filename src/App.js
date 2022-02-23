7e69b51fafed04135e64dc6a6296f54a9440a748
robisonwebdev <david@robisonweb.dev>
2022-02-23 11:39:51 -0700

    Updated Main import path.

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import Header from './components/Header/Header';
-import Main from './components/Main';
+import Main from './components/Main/Main';
 import Footer from './components/Footer/Footer';
 import './styles/App.css';
 
