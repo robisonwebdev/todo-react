00b32cb65a08cea7c3c33b3a24ee824190e66d22
robisonwebdev <david@robisonweb.dev>
2022-02-24 11:57:50 -0700

    Changed return div's className to .project Changed projectItem className to .project_name

--- src/components/Main/Project.js
+++ src/components/Main/Project.js
@@ -3,8 +3,8 @@ import '../../styles/Main/Project.css';
 
 const Project = ({ deleteProject, projectID, text, viewProject }) => {
     return (
-        <div className='Project'>
-            <div className='projectItem' onClick={() => viewProject(projectID)}>
+        <div className='project'>
+            <div className='project_name' onClick={() => viewProject(projectID)}>
                 <i className="las la-tasks"></i>
                 <p>{text}</p>
             </div>            
