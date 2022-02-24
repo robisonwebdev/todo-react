ec6ff04c8ff9c4100122204ffcc22e414d34de32
robisonwebdev <david@robisonweb.dev>
2022-02-24 09:26:29 -0700

    Updated ProjectsContainer import path.

--- src/components/Main/Main.js
+++ src/components/Main/Main.js
@@ -1,5 +1,5 @@
 import React, { useEffect, useState } from 'react';
-import ProjectsContainer from '../ProjectsContainer';
+import ProjectsContainer from './ProjectsContainer';
 import ListsContainer from './ListsContainer';
 import '../../styles/Main/Main.css';
 
