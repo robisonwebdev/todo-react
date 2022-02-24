2299410aa7f7b636d6cab3512512f272305a3cc0
robisonwebdev <david@robisonweb.dev>
2022-02-24 10:38:22 -0700

    Refactored Component.

--- src/components/Main/ProjectsContainer.js
+++ src/components/Main/ProjectsContainer.js
@@ -4,42 +4,65 @@ import Button from '../Button';
 import AddProjectForm from '../AddProjectForm';
 import '../../styles/Main/ProjectsContainer.css';
 
-const ProjectsContainer = ({ addProject, deleteProject, projects, viewProject }) => {
-    const [inputValue, setInputValue] = useState('');
-    const [addProjectBtn, setAddProjectBtn] = useState(true);
-    const [showForm, setShowForm] = useState(false);
+const ProjectsContainer = () => {
+    const [projects, setProjects] = useState([]);
+    const [showAddProjectBtn, setShowAddProjectBtn] = useState(true);
+    const [showProjectForm, setShowProjectForm] = useState(false);
 
-    const projectsMap = projects.map(project => <Project key={project.title} text={project.title} projectID={project.id} viewProject={viewProject} deleteProject={deleteProject} />)
+    const addProject = (value) => {
+        const newObject = {
+            title: value,
+            items: [],
+            id: Math.floor(Math.random() * 10000)
+        };
 
-    const handleAddProjectBtn = () => {
-        setAddProjectBtn(false);
-        setShowForm(true);
+        if (value === '') return null;
+
+        setProjects(prevState => [...prevState, newObject]);
+    };
+
+    const deleteProject = (id) => {
+        const newArray = projects.filter(project => project.id !== id);
+
+        setProjects(newArray);
+    };
+
+    const displayProjects = () => {
+        const mapProjects = projects.map(project => {
+            return (
+                <Project
+                    deleteProject={deleteProject}
+                    key={project.id}
+                    projectID={project.id}
+                    text={project.title}
+                />
+            );
+        });
+
+        return mapProjects;
     }
 
-    const handleCancel = () => {
-        setInputValue('');
-        setShowForm(false);
-        setAddProjectBtn(true);
-    }
+    const handleFormReset = () => {
+        setShowAddProjectBtn(true);
+        setShowProjectForm(false);
+    };
 
-    const handleChange = (event) => {
-        setInputValue(event.target.value);
-    }
+    const getNewProject = (value) => {
+        addProject(value);
+        handleFormReset();
+    };
 
-    const handleSubmit = (event) => {
-        event.preventDefault();
-        addProject(inputValue);
-        setInputValue('');
-        setShowForm(false);
-        setAddProjectBtn(true);
-    }
+    const handleShowingForm = () => {
+        setShowAddProjectBtn(false);
+        setShowProjectForm(true);
+    };
 
     return (
         <section className='projects_container'>
             <h2>Projects</h2>
-            {projectsMap}
-            {addProjectBtn ? <Button className='addProjectBtn' onClick={handleAddProjectBtn} text='Add Project' /> : null}
-            {showForm ? <AddProjectForm onSubmit={handleSubmit} onChange={handleChange} onCancel={handleCancel} value={inputValue} /> : null }
+            {projects.length === 0 ? <p>No Projects</p> : displayProjects()}
+            {showAddProjectBtn ? <Button className='addProjectBtn' onClick={handleShowingForm} text='Add Project' /> : null}
+            {showProjectForm ? <AddProjectForm onCancel={handleFormReset} newProject={getNewProject} /> : null}
         </section>
     );
 }
