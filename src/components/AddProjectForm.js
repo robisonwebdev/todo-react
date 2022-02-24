70a0766ca6c2dd22914261fb321c84179c93ed06
robisonwebdev <david@robisonweb.dev>
2022-02-24 10:11:31 -0700

    Imported useState. Removed onChange and value from destructured props. Setup inputValue state, with an initial value of empty string. Setup handleInputChange function, to update inputValue state. Changed onSubmit form event, to pass inputValue back up using onSubmit prop.

--- src/components/AddProjectForm.js
+++ src/components/AddProjectForm.js
@@ -1,10 +1,16 @@
-import React from 'react';
+import React, { useState } from 'react';
 import '../styles/AddProjectForm.css';
 
-const AddProjectForm = ({ onChange, onCancel, onSubmit, value}) => {
+const AddProjectForm = ({ onCancel, onSubmit }) => {
+    const [inputValue, setInputValue] = useState('');
+
+    const handleInputChange = (event) => {
+        setInputValue(event.target.value);
+    };
+
     return (
-        <form className='projectForm' onSubmit={onSubmit}>
-                <input type='text' value={value} onChange={onChange} placeholder='Project Name' />
+        <form className='projectForm' onSubmit={() => onSubmit(inputValue)}>
+                <input type='text' value={inputValue} onChange={handleInputChange} placeholder='Project Name' />
                 <div className='projectFormBtns'>
                     <button type='submit'>Add Project</button>
                     <button type='button' onClick={onCancel}>Cancel</button>
