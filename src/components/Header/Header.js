fd4b4174d93de42484cb4daae439f3b621696cf1
robisonwebdev <david@robisonweb.dev>
2022-02-25 12:48:20 -0700

    Replaced i tag icon with CheckBoxSharpIcon

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,10 +1,11 @@
 import React from 'react';
+import CheckBoxSharpIcon from '@mui/icons-material/CheckBoxSharp';
 import '../../styles/Header/Header.css';
 
 const Header = () => {
     return (
         <header>
-            <i className="las la-check-circle"></i>
+            <CheckBoxSharpIcon sx={{ fontSize: 35}} />
             <h1>Todo List</h1>
         </header>
     );
