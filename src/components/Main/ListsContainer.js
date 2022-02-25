5a2777efd5145d9c41a3bef4696699d4c4c5775f
robisonwebdev <david@robisonweb.dev>
2022-02-25 13:35:07 -0700

    Commented out most code.

--- src/components/Main/ListsContainer.js
+++ src/components/Main/ListsContainer.js
@@ -1,69 +1,69 @@
 import React, { useEffect, useState } from 'react';
 import Checkbox from '../Checkbox';
-import Button from '../Button';
+import Button from './Button';
 import DisplayChecked from '../DisplayChecked';
 import AddItemForm from '../AddItemForm';
 import '../../styles/Main/ListsContainer.css';
 
 const ListsContainer = ({ addItem, deleteChecked, deleteItem, id, items, title, uncheckAll, updateChecked }) => {
-    const [inputValue, setInputVale] = useState('');
-    const [showBtn, setShowBtn] = useState(true);
-    const [showForm, setShowForm] = useState(false);
-    const [showChecked, setShowChecked] = useState(false);
+    // const [inputValue, setInputVale] = useState('');
+    // const [showBtn, setShowBtn] = useState(true);
+    // const [showForm, setShowForm] = useState(false);
+    // const [showChecked, setShowChecked] = useState(false);
 
-    useEffect(() => {
-        handleReset();
-    }, [id])
+    // useEffect(() => {
+    //     handleReset();
+    // }, [id])
 
-    useEffect(() => {
-        handleShowChecked();
-    }, [items])
+    // useEffect(() => {
+    //     handleShowChecked();
+    // }, [items])
 
-    let displayUnchecked = items.map(item => {
-        if (!item.checked) {
-            return <Checkbox
-                checked={item.checked}
-                className='item'
-                iconName='las la-trash'
-                id={id}
-                key={item.name + id}
-                name={item.name}
-                onChange={updateChecked}
-                onClick={deleteItem}
-            />
-        } else return null;        
-    });    
+    // let displayUnchecked = items.map(item => {
+    //     if (!item.checked) {
+    //         return <Checkbox
+    //             checked={item.checked}
+    //             className='item'
+    //             iconName='las la-trash'
+    //             id={id}
+    //             key={item.name + id}
+    //             name={item.name}
+    //             onChange={updateChecked}
+    //             onClick={deleteItem}
+    //         />
+    //     } else return null;        
+    // });    
 
-    const handleAddItemBtn = () => {
-        setShowBtn(false);
-        setShowForm(true);
-    }
+    // const handleAddItemBtn = () => {
+    //     setShowBtn(false);
+    //     setShowForm(true);
+    // }
 
-    const handleChange = (event) => {
-        setInputVale(event.target.value);
-    }
+    // const handleChange = (event) => {
+    //     setInputVale(event.target.value);
+    // }
 
-    const handleReset = () => {
-        setShowBtn(true);
-        setShowForm(false);
-        setInputVale('');
-    }
+    // const handleReset = () => {
+    //     setShowBtn(true);
+    //     setShowForm(false);
+    //     setInputVale('');
+    // }
 
-    const handleShowChecked = () => {
-        const results = items.filter(item => item.checked === true);
+    // const handleShowChecked = () => {
+    //     const results = items.filter(item => item.checked === true);
 
-        results.length > 0 ? setShowChecked(true) : setShowChecked(false);
-    }
+    //     results.length > 0 ? setShowChecked(true) : setShowChecked(false);
+    // }
 
-    const handleSubmit = (event) => {
-        event.preventDefault();
-        addItem(id, inputValue);
-        handleReset();
-    }
+    // const handleSubmit = (event) => {
+    //     event.preventDefault();
+    //     addItem(id, inputValue);
+    //     handleReset();
+    // }
     
     return (
         <section className='lists_container'>
-            <div className='displayUnchecked'>
+            {/* <div className='displayUnchecked'>
                 <h1>{title}</h1>
                 <div className='listItems'>
                     {displayUnchecked}
@@ -71,7 +71,7 @@ const ListsContainer = ({ addItem, deleteChecked, deleteItem, id, items, title, 
                 {showBtn ? <Button onClick={handleAddItemBtn} className='listButton' text='Add Item' /> : null}
                 {showForm ? <AddItemForm submit={handleSubmit} cancel={handleReset} handleChange={handleChange} value={inputValue} /> : null}
             </div>
-            {showChecked ? <DisplayChecked id={id} items={items} deleteChecked={() => deleteChecked(id)} uncheckAll={() => uncheckAll(id)} updateChecked={updateChecked} /> : null }
+            {showChecked ? <DisplayChecked id={id} items={items} deleteChecked={() => deleteChecked(id)} uncheckAll={() => uncheckAll(id)} updateChecked={updateChecked} /> : null } */}
         </section>
     );
 }
