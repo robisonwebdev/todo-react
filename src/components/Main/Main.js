093288f832ff0d6909eceaa00bdd78bb63857aea
robisonwebdev <david@robisonweb.dev>
2022-02-25 13:35:27 -0700

    Commented out most code.

--- src/components/Main/Main.js
+++ src/components/Main/Main.js
@@ -4,132 +4,136 @@ import ListsContainer from './ListsContainer';
 import '../../styles/Main/Main.css';
 
 const Main = () => {
-    const [projects, setProjects] = useState([
-        {
-          title: 'Shopping List',
-          items: [
-              {
-                  name: 'Milk',
-                  checked: false
-              },
-              {
-                  name: 'Bread',
-                  checked: false
-              },
-              {
-                name: 'Eggs',
-                checked: false
-            },
-            {
-                name: 'Butter',
-                checked: true
-            },
-            {
-                name: 'Potatos',
-                checked: false
-            }
-          ],
-          id: 147
-        }
-    ]);
-    const [currentProject, setCurrentProject] = useState(projects[0]);
-    const [currentID, setCurrentID] = useState(projects[0].id)
+    const [projects, setProjects] = useState([]);
 
-    useEffect(() => {
-        viewProject(currentID);
-    }, [projects, currentID])
+    // const [projects, setProjects] = useState([
+    //     {
+    //       title: 'Shopping List',
+    //       items: [
+    //           {
+    //               name: 'Milk',
+    //               checked: false
+    //           },
+    //           {
+    //               name: 'Bread',
+    //               checked: false
+    //           },
+    //           {
+    //             name: 'Eggs',
+    //             checked: false
+    //         },
+    //         {
+    //             name: 'Butter',
+    //             checked: true
+    //         },
+    //         {
+    //             name: 'Potatos',
+    //             checked: false
+    //         }
+    //       ],
+    //       id: 147
+    //     }
+    // ]);
+    // const [currentProject, setCurrentProject] = useState(projects[0]);
+    // const [currentID, setCurrentID] = useState(projects[0].id)
+
+    // useEffect(() => {
+    //     viewProject(currentID);
+    // }, [projects, currentID])
     
-    const addItem = (id, value) => {
-        let updateItems = projects.map(project => {
-            if (project.id === id) {
-                return {...project, items: [...project.items, {
-                    name: value,
-                    checked: false
-                }]};
-            }
-            return project;
-        })
+    // const addItem = (id, value) => {
+    //     let updateItems = projects.map(project => {
+    //         if (project.id === id) {
+    //             return {...project, items: [...project.items, {
+    //                 name: value,
+    //                 checked: false
+    //             }]};
+    //         }
+    //         return project;
+    //     })
 
-        setProjects(updateItems);
-    }
+    //     setProjects(updateItems);
+    // }
     
-    const addProject = (value) => {
-        let newObject = {
-            title: value,
-            items: [],
-            id: Math.floor(Math.random() * 10000)
-        }
+    // const addProject = (value) => {
+    //     let newObject = {
+    //         title: value,
+    //         items: [],
+    //         id: Math.floor(Math.random() * 10000)
+    //     }
 
-        if (value === '') {
-            return null;
-        } else {
-            setProjects(prevState => [...prevState, newObject]);
-        }
-    }
+    //     if (value === '') {
+    //         return null;
+    //     } else {
+    //         setProjects(prevState => [...prevState, newObject]);
+    //     }
+    // }
 
-    const deleteChecked = (id) => {
-        let newArray = currentProject.items.filter(item => {
-            return item.checked !== true;
-        })
+    // const deleteChecked = (id) => {
+    //     let newArray = currentProject.items.filter(item => {
+    //         return item.checked !== true;
+    //     })
 
-        updateProjects(id, newArray);
-    }
+    //     updateProjects(id, newArray);
+    // }
 
-    const deleteItem = (id, name) => {
-        let newArray = currentProject.items.filter(item => item.name !== name);
+    // const deleteItem = (id, name) => {
+    //     let newArray = currentProject.items.filter(item => item.name !== name);
 
-        updateProjects(id, newArray);
-    }
+    //     updateProjects(id, newArray);
+    // }
 
-    const deleteProject = (id) => {
-        const newArray = projects.filter(project => project.id !== id);
+    // const deleteProject = (id) => {
+    //     const newArray = projects.filter(project => project.id !== id);
 
-        setProjects(newArray);
-    }
+    //     setProjects(newArray);
+    // }
 
-    const uncheckAll = (id) => {
-        let newArray = currentProject.items.map(item => {
-            if (item.checked === true) {
-                return {...item, checked: false}
-            }
-            return item;
-        })
+    // const uncheckAll = (id) => {
+    //     let newArray = currentProject.items.map(item => {
+    //         if (item.checked === true) {
+    //             return {...item, checked: false}
+    //         }
+    //         return item;
+    //     })
 
-        updateProjects(id, newArray);
-    }
+    //     updateProjects(id, newArray);
+    // }
 
-    const updateChecked = (id, name) => {
-        let newArray = currentProject.items.map(item => {
-            if (item.name === name) {
-                return {...item, checked: !item.checked}
-            }
-            return item;
-        })
+    // const updateChecked = (id, name) => {
+    //     let newArray = currentProject.items.map(item => {
+    //         if (item.name === name) {
+    //             return {...item, checked: !item.checked}
+    //         }
+    //         return item;
+    //     })
  
-        updateProjects(id, newArray);
-    }
+    //     updateProjects(id, newArray);
+    // }
 
-    const updateProjects = (id, newArray) => {
-        let updateProjects = projects.map(project => {
-            if (project.id === id) {
-                return {...project, items: newArray};
-            }
-            return project;
-        })
+    // const updateProjects = (id, newArray) => {
+    //     let updateProjects = projects.map(project => {
+    //         if (project.id === id) {
+    //             return {...project, items: newArray};
+    //         }
+    //         return project;
+    //     })
 
-        setProjects(updateProjects);
-    }
+    //     setProjects(updateProjects);
+    // }
 
-    const viewProject = (id) => {
-        let findProject = projects.filter(project => project.id === id);
+    // const viewProject = (id) => {
+    //     let findProject = projects.filter(project => project.id === id);
 
-        setCurrentProject(findProject[0]);
-        setCurrentID(findProject[0].id);
-    }
+    //     setCurrentProject(findProject[0]);
+    //     setCurrentID(findProject[0].id);
+    // }
 
     return (
         <main>
-            <ProjectsContainer projects={projects} addProject={addProject} deleteProject={deleteProject} viewProject={viewProject} />
+            <ProjectsContainer projects={projects} setProjects={setProjects} />
+            <ListsContainer />
+            {/* <ProjectsContainer projects={projects} addProject={addProject} deleteProject={deleteProject} viewProject={viewProject} />
             <ListsContainer
                 project={currentProject}
                 id={currentProject.id}
@@ -140,7 +144,7 @@ const Main = () => {
                 deleteItem={deleteItem}
                 uncheckAll={uncheckAll}
                 updateChecked={updateChecked}
-            />
+            /> */}
         </main>
     );
 }
