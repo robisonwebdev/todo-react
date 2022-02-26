9b3783bb38e52278d2863c71dffe3e35d1d78a14
robisonwebdev <david@robisonweb.dev>
2022-02-26 16:31:30 -0700

    Destrucutred project props. Setup function called getItems to get all unchecked items. Setup h1 in return and passed it title. Setup ternery in return to check item length and return meesage or getItems function.'

--- src/components/Main/CheckedList.js
+++ src/components/Main/CheckedList.js
@@ -1,8 +1,37 @@
 import React from 'react';
+import Checkbox from '../Checkbox';
 
-const CheckedList = () => {
+const CheckedList = ({ project }) => {
+    const { items, title } = project[0];
+
+    const getItems = () => {
+        const mapItems = items.map(item => {
+            if (item.checked) return null;
+    
+            return (
+                <li>
+                    <Checkbox
+                        checked={item.checked}
+                        className='item'
+                        iconName='las la-trash'
+                        // id={id}
+                        // key={item.name + id}
+                        name={item.name}
+                        // onChange={updateChecked}
+                        // onClick={deleteItem}
+                    />
+                </li>
+            );
+        });
+
+        return mapItems;
+    };
+
     return (
-        <ul>CheckedList</ul>
+        <>
+            <h1>{title}</h1>
+            {items.length === 0 ? <p>No items in list.</p> : getItems()}
+        </>
     );
 };
 
