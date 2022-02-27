5c59c75dfe634d2580d4c7b6107d21b83cd2ea4d
robisonwebdev <david@robisonweb.dev>
2022-02-27 11:07:03 -0700

    Changed label to an li. Changed .itemLeft to .item_left Changed .itemRight to .item_right

--- src/components/Main/Checkbox.js
+++ src/components/Main/Checkbox.js
@@ -4,15 +4,15 @@ import '../../styles/Main/Checkbox.css';
 
 const Checkbox = ({ className, checked, iconName, id, onChange, onClick, name }) => {
     return (
-        <label className={className}>
-            <div className='itemLeft'>
+        <li className={className}>
+            <div className='item_left'>
                 <input type='checkbox' checked={checked} onChange={() => onChange(id, name)} />
                 {name}
             </div>
-            <div className='itemRight'>
+            <div className='item_right'>
                 <DeleteForeverIcon sx={{ fontSize: 20 }} />
             </div>
-        </label>
+        </li>
     );
 }
 
