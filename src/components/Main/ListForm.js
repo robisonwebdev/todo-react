1770b3c86bb255d0781abd859c6978ef2d82e2df
robisonwebdev <david@robisonweb.dev>
2022-03-01 09:33:27 -0700

    Setup handleSubmit function. Added preventDefault to handleSubmit. Changed form onSubmit event to handleSubmit.

--- src/components/Main/ListForm.js
+++ src/components/Main/ListForm.js
@@ -2,8 +2,13 @@ import React from 'react';
 import '../../styles/Main/ListForm.css';
 
 const ListForm = ({ cancel, handleChange, submit, value }) => {
+
+    const handleSubmit = (event) => {
+        event.preventDefault();
+    }
+
     return (
-        <form className='list_form' onSubmit={submit}>
+        <form className='list_form' onSubmit={handleSubmit}>
             <input type='text' onChange={handleChange} value={value} placeholder='Enter Item' />
             <div className='form_controls'>
                 <button type='submit'>Add</button>
