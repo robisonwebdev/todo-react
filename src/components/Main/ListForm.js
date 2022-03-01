012c30b9c2b8575f74960faef27484c99464b881
robisonwebdev <david@robisonweb.dev>
2022-03-01 10:28:06 -0700

    Imported useState. Added props of addListItem and onCancel. Set state for inputValue, with an inital value of empty string. Setup two new functions. handleInputChange and handleSubmit. Added addListItem and setInputValue to handleSubmit. changed input onChanged event to handleInputChange. Changed input value to inputValue. Changed cancel button onClick event to onCancel.

--- src/components/Main/ListForm.js
+++ src/components/Main/ListForm.js
@@ -1,18 +1,25 @@
-import React from 'react';
+import React, { useState } from 'react';
 import '../../styles/Main/ListForm.css';
 
-const ListForm = ({ cancel, handleChange, submit, value }) => {
+const ListForm = ({ addListItem, onCancel, handleChange, submit, value }) => {
+    const [inputValue, setInputValue] = useState('');
+
+    const handleInputChange = (event) => {
+        setInputValue(event.target.value);
+    };
 
     const handleSubmit = (event) => {
         event.preventDefault();
+        addListItem(inputValue);
+        setInputValue('');
     }
 
     return (
         <form className='list_form' onSubmit={handleSubmit}>
-            <input type='text' onChange={handleChange} value={value} placeholder='Enter Item' />
+            <input type='text' onChange={handleInputChange} value={inputValue} placeholder='Enter Item' />
             <div className='form_controls'>
                 <button type='submit'>Add</button>
-                <button onClick={cancel}>Cancel</button>
+                <button onClick={onCancel}>Cancel</button>
             </div>
         </form>
     );
