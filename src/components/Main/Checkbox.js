f627deabca73b23d43c4f7637609fa9d23a7bb8c
robisonwebdev <david@robisonweb.dev>
2022-03-02 11:58:06 -0700

    Removed unused props to component. Added handleCheckbox prop to component. Changed prop passed to onChange (in input), to handleCheckbox.

--- src/components/Main/Checkbox.js
+++ src/components/Main/Checkbox.js
@@ -2,14 +2,14 @@ import React from 'react';
 import DeleteForeverIcon from '@mui/icons-material/DeleteForever';
 import '../../styles/Main/Checkbox.css';
 
-const Checkbox = ({ className, checked, iconName, id, onChange, onClick, name }) => {
+const Checkbox = ({ checked, className, deleteListItem, handleCheckbox, name }) => {
     return (
         <li className={className}>
             <div className='item_left'>
-                <input type='checkbox' checked={checked} onChange={() => onChange(id, name)} />
+                <input type='checkbox' checked={checked} onChange={() => handleCheckbox(name)} />
                 {name}
             </div>
-            <div className='item_right'>
+            <div className='item_right' onClick={() => deleteListItem(name)}>
                 <DeleteForeverIcon sx={{ fontSize: 20 }} />
             </div>
         </li>
