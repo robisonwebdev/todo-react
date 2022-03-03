af531c7c655b465897b9013ed8d5f41b3897a287
robisonwebdev <david@robisonweb.dev>
2022-03-03 10:27:59 -0700

    Imported useEffect, useState, and CheckedContainer.js Setup state for showCheckedContainer, and set to an intial value of false. Setup useEffect to look for to see it project items checked equal true. Calls setShowCheckedContainer and passes true or false based on result. Added second ternary op for CheckedContainer. Now checks for project and showCheckedContainer bool values.

--- src/components/Main/ListsContainer.js
+++ src/components/Main/ListsContainer.js
@@ -1,20 +1,36 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import CheckedContainer from './CheckedContainer';
 import UncheckedContainer from './UncheckedContainer';
 import '../../styles/Main/ListsContainer.css';
 
 
 const ListsContainer = ({ allProjects, project, updateAllProjects }) => {
+    const [showCheckedContainer, setShowCheckedContainer] = useState(false);
+
     const noProjectMessage = () => {
         return (
             <p>No Project selected. Click on a project to view todo list.</p>
         );
     };
 
+    // Checks project items for checked = true, sets state based on result
+    useEffect(() => {
+        if (project !== null) {
+            const filterChecked = project[0].items.filter(item => item.checked === true);
+
+            filterChecked.length > 0 ? setShowCheckedContainer(true) : setShowCheckedContainer(false);
+        };
+    }, [allProjects, project]);
+
     return (
         <section className='lists_container'>
             {project === null ? noProjectMessage() : null}
             {project === null ? null : <UncheckedContainer allProjects={allProjects} project={project} updateAllProjects={updateAllProjects} />}
-            {/* {project === null ? null : <CheckedContainer allProjects={allProjects} project={project} updateAllProjects={updateAllProjects} />} */}
+            {project === null 
+                ? null
+                : showCheckedContainer ? <CheckedContainer allProjects={allProjects} project={project} updateAllProjects={updateAllProjects} />
+                    : null
+            }
         </section>
     );
 }
