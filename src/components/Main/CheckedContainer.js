9df5c0f6dc08a9fdffa671f9f14518e5f5937b8a
robisonwebdev <david@robisonweb.dev>
2022-03-03 11:42:52 -0700

    Imported Button.js, CheckedList.js, and CheckedContainer.css Added destructured props. Gave section a className of .checked_container Added h1 with 'Completed List' text. Added div with a className of .checked_list_buttons. Added two Button components. Added CheckedList component.

--- src/components/Main/CheckedContainer.js
+++ src/components/Main/CheckedContainer.js
@@ -1,8 +1,18 @@
 import React from 'react';
+import Button from './Button';
+import CheckedList from './CheckedList';
+import '../../styles/Main/CheckedContainer.css';
 
-const CheckedContainer = () => {
+const CheckedContainer = ({ allProjects, project, updatedAllProjects }) => {
     return (
-        <section>CheckedContainer</section>
+        <section className='checked_container'>
+            <h1>Completed List</h1>
+            <div className='checked_list_buttons'>
+                <Button text='Uncheck All' />
+                <Button text='Delete All' />
+            </div>
+            <CheckedList project={project} />
+        </section>
     );
 };
 
