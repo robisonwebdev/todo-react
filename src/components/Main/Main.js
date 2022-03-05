05c7dd839762e482cbffdd248c6b09094b4384ab
robisonwebdev <david@robisonweb.dev>
2022-03-05 08:48:00 -0700

    Removed commented code. Removed useEffect import.

--- src/components/Main/Main.js
+++ src/components/Main/Main.js
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from 'react';
+import React, { useState } from 'react';
 import ProjectsContainer from './ProjectsContainer';
 import ListsContainer from './ListsContainer';
 import '../../styles/Main/Main.css';
@@ -40,74 +40,9 @@ const Main = () => {
         return findProject;
     };
 
-    // useEffect(() => {
-    //     viewProject(currentID);
-    // }, [projects, currentID])
-    
-    // const addItem = (id, value) => {
-    //     let updateItems = projects.map(project => {
-    //         if (project.id === id) {
-    //             return {...project, items: [...project.items, {
-    //                 name: value,
-    //                 checked: false
-    //             }]};
-    //         }
-    //         return project;
-    //     })
-
-    //     setProjects(updateItems);
-    // }
-
-    // const deleteChecked = (id) => {
-    //     let newArray = currentProject.items.filter(item => {
-    //         return item.checked !== true;
-    //     })
-
-    //     updateProjects(id, newArray);
-    // }
-
-    // const deleteItem = (id, name) => {
-    //     let newArray = currentProject.items.filter(item => item.name !== name);
-
-    //     updateProjects(id, newArray);
-    // }
-
-    // const uncheckAll = (id) => {
-    //     let newArray = currentProject.items.map(item => {
-    //         if (item.checked === true) {
-    //             return {...item, checked: false}
-    //         }
-    //         return item;
-    //     })
-
-    //     updateProjects(id, newArray);
-    // }
-
-    // const updateChecked = (id, name) => {
-    //     let newArray = currentProject.items.map(item => {
-    //         if (item.name === name) {
-    //             return {...item, checked: !item.checked}
-    //         }
-    //         return item;
-    //     })
- 
-    //     updateProjects(id, newArray);
-    // }
-
-    // const updateProjects = (id, newArray) => {
-    //     let updateProjects = projects.map(project => {
-    //         if (project.id === id) {
-    //             return {...project, items: newArray};
-    //         }
-    //         return project;
-    //     })
-
-    //     setProjects(updateProjects);
-    // }
-
     return (
         <main>
-            <ProjectsContainer projects={projects} setProjects={setProjects} setSelectedProject={setSelectedProject} />
+            <ProjectsContainer projects={projects} selectedProject={selectedProject} setProjects={setProjects} setSelectedProject={setSelectedProject} />
             <ListsContainer allProjects={projects} project={displayProject()} updateAllProjects={setProjects} />
         </main>
     );
